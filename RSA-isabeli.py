d56cf24e5f9c9a13db7fdbdea3005f32fdf5deea
isabelirk <31291959+isabelirk@users.noreply.github.com>
2022-02-05 19:26:10 -0300

    rsa inicial

--- RSA-isabeli.py
+++ RSA-isabeli.py
@@ -1,6 +1,27 @@
+import math
+
+from sympy import Q
+
 print('#############################')
 print('# O que deseja fazer?       #')
 print('# 1 - Criar chave           #')
 print('# 2 - Cifrar mensagem       #')
 print('# 3 - Descifrar mensagem    #')
 print('#############################')
+
+message = int(input('Digite a mensagem a ser criptografaga: '))5
+
+p = 11
+q = 7
+e = 3
+
+n = p * q
+
+def encrypt(me):
+    en = math.pow(me, e)
+    c = en % n
+    print('Encriptando a mensagem: ', c)
+    return c
+
+print ('Mensagem original: ', message)
+c = encrypt(message)
