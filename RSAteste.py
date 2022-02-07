94316644755427dd3cfbaecac19d1960db796587
Isabeli Reik <isabelireik2@gmail.com>
2022-02-07 11:21:17 -0300

    função primo

--- RSAteste.py
+++ RSAteste.py
@@ -10,3 +10,36 @@ print('#############################')
 message = int(input())
 
 print('Opção digitada: ', message)
+
+# Verifica se um numero gerado é primo
+def prime(n): 
+    if (n <= 1):
+        return False
+    if (n <= 3):
+        return True
+
+    if (n%2 == 0 or n%3 == 0):
+        return False
+
+    i = 5
+    while(i * i <= n):
+        if (n%i == 0 or n%(i+2) == 0):
+            return False
+        i+=6
+    return True
+
+def verificaPrimo(primo):
+    if(prime(primo) == True):
+        print('É primo!')
+    else:
+        print('Não é primo!')
+
+#def gerarChavePublicaPrivada (primo):
+
+if(message == 1):
+    print("Por favor digite dois números primos:")
+    primoA = int(input())
+    primoB = int(input())
+
+    verificaPrimo(primoA)
+    verificaPrimo(primoB)
